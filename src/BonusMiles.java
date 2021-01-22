b6106b76d7793f5ca8178fedac9c35228c6f1136
OksanaTarun <oxana.tarun@gmail.com>
2021-01-22 12:57:17 +0300

    BonusMiles added

---
+++ src/BonusMiles.java
@@ -0,0 +1,9 @@
+public class BonusMiles {
+    public static void main(String[] args) {
+        long ticketPrice = 5000;
+        long pricePerMile = 20;
+        long bonusMiles = ticketPrice / pricePerMile;
+        System.out.println(bonusMiles);
+    }
+}
+
